f68fde30bfcb7ca9dbec5a52c04a40db05e27afd
moises <moises@gmail.com>
2025-06-20 13:06:27 -0600

    change product

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 app.MapGet("/products", () =>
     {
-        return Results.Ok(new[] {"Product1", "Product2", "Product3"});
+        return Results.Ok(new[] {"Product1", "Product2", "Product3.1"});
     })
     .WithName("GetProducts")
     .WithOpenApi();
